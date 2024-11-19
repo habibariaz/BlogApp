c1c7c65be080b78d219ac61949994c7a0d73d8f2
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-11-19 17:55:54 +0500

    Update Api.js

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -106,8 +106,12 @@ for (const [key, value] of Object.entries(SERVICE_URL)) {
             data: value.method === "DELETE" ? {} : body,
             responseType: value.responseType || 'json', // Default responseType
             //authenticate access token
-            headers: {
-                authorization: getAccessToken()
+            // headers: {
+            //     // authorization: getAccessToken()
+                
+            // },
+             headers: {
+                authorization: `Bearer ${getAccessToken()}`, // Corrected format
             },
             TYPE: getType(value, body),
 
