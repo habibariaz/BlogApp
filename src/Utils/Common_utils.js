b88d756b89455f856ab0c37a3440ef788861e61b
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-11-19 18:52:54 +0500

    Update Common_utils.js

--- src/Utils/Common_utils.js
+++ src/Utils/Common_utils.js
@@ -3,7 +3,7 @@
 //     return sessionStorage.getItem('accessToken')
 // }
 export const getAccessToken = () => {
-    return sessionStorage.getItem('accessToken') || null;
+    return sessionStorage.getItem('accessToken');
 }; 
 export const addElipsis = (str, limit) => {
     return str.length > limit ? str.substring(0, limit) + '...' : str;
